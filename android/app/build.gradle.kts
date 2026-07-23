7a13abd3c52d8d82fcb3e18f6a8f25ce6e64f32e
Nizamudeen <nizamudeen.abdulkather@spantagtech.com>
2026-07-23 14:02:33 +0530

    Version updated for release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -5,10 +8,18 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
     namespace = "com.seventaxi.customer"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
+
     defaultConfig {
         applicationId = "com.seventaxi.customer"
         minSdk = flutter.minSdkVersion
@@ -18,25 +29,32 @@ android {
         multiDexEnabled = true
     }
 
-    // ✅ No custom signing config
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("debug") // ✅ Use debug signing for release
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
         }
     }
 
     compileOptions {
-    isCoreLibraryDesugaringEnabled = true
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
-kotlinOptions {
-    jvmTarget = "17"
-}
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 flutter {
